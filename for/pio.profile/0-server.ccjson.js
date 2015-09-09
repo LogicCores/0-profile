066f135d6c49baee30710d110950136763239417
Christoph Dorn <christoph@christophdorn.com>
2015-09-08 17:02:09 -0700

    initial

---
+++ for/pio.profile/0-server.ccjson.js
@@ -0,0 +1,34 @@
+
+exports.forLib = function (LIB) {
+
+    const PROFILE = require("../../../../lib/pio.profile/0-server.api").forLib(LIB);
+
+    return LIB.Promise.resolve({
+        forConfig: function (defaultConfig) {
+
+            var Entity = function (instanceConfig) {
+                var self = this;
+                var config = {};
+                LIB._.merge(config, defaultConfig)
+                LIB._.merge(config, instanceConfig)
+
+                self.AspectInstance = function (aspectConfig) {
+
+                    return LIB.Promise.resolve({
+                        decrypt: function () {
+                            return PROFILE.decrypt(
+                                config.env,
+                                JSON.stringify(aspectConfig)
+                            ).then(function (aspectConfig) {
+                                return JSON.parse(aspectConfig);
+                            });
+                        }
+                    });
+                }
+            }
+            Entity.prototype.config = defaultConfig;
+
+            return Entity;
+        }
+    });
+}
---
+++ for/pio.profile/encrypt.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+if [ -z "$HOME" ]; then
+	echo "ERROR: 'HOME' environment variable is not set!"
+	exit 1
+fi
+# Source https://github.com/bash-origin/bash.origin
+. "$HOME/.bash.origin"
+function init {
+	eval BO_SELF_BASH_SOURCE="$BO_READ_SELF_BASH_SOURCE"
+	BO_deriveSelfDir ___TMP___ "$BO_SELF_BASH_SOURCE"
+	local __BO_DIR__="$___TMP___"
+
+    export VERBOSE="1"
+
+	function Encrypt {
+		BO_format "$VERBOSE" "HEADER" "Encrypting new profile variables if found ..."
+
+    	if [ -z "$PIO_PROFILE_KEY" ]; then
+    	    echo "The 'PIO_PROFILE_KEY' environment variable must be set!"
+    	    exit 1
+    	fi
+    	if [ -z "$PIO_PROFILE_SECRET" ]; then
+    	    echo "The 'PIO_PROFILE_SECRET' environment variable must be set!"
+    	    exit 1
+    	fi
+
+        # TODO: Make this search path configurable
+        pushd "$__BO_DIR__/../../../../data/0/Deployments" > /dev/null
+            for dir in $(find . -type d); do
+                "$__BO_DIR__/../../../../lib/pio.profile/bin/pio-profile-encrypt" \
+                    "$dir/profile.ccjson"
+            done
+        popd > /dev/null
+
+		BO_format "$VERBOSE" "FOOTER"
+	}
+
+	Encrypt $@
+
+}
+init $@
